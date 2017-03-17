2b40f5beb479d3aad08b2e438803443e56db28dc
Charles Vandevoorde <charles.vandevoorde@hotmail.be>
2017-03-17 10:31:16 +0100

    m

--- code/raspberrypi/map/graphmap.py
+++ code/raspberrypi/map/graphmap.py
@@ -1,26 +1,25 @@
 import math
+import os.path
 
 import matplotlib.pyplot as plt
 import networkx as nx
 
 class GraphMap:
-    def __init__(self, nodes, triangle_cells):
-        self._graph = self.__build_graph(nodes, triangle_cells)
-        self.__merge_nodes(20, 300)
-        self.__clean()
+    def __init__(self, nodes, triangles, cache=False):
+        """
+        nodes represent the (x, y) address of nodes in the graph
+        triangles give the 3 positions in nodes array to form a triangle
+        """
+        self._cache = cache
+        self._cache_path = '/tmp/graphmap.data'
 
-    def display(self):
-        print(nx.get_node_attributes(self._graph, 'color').values())
-        nx.draw_networkx(self._graph, nx.get_node_attributes(self._graph, 'pos'),
-                node_color=nx.get_node_attributes(self._graph, 'color').values())
-        plt.show()
+        self._graph = None
+        if self._cache and os.path.exists(self._cache_path):
+            self._graph = self.read_cache()
+        else:
+            self.__build_graph_from_mesh(nodes, triangles)
 
-    def __distance_btw_points(self, p1, p2):
-        x = (p1[0] - p2[0])**2
-        y = (p1[1] - p2[1])**2
-        return math.sqrt(x + y)
-
-    def __build_graph(self, nodes, triangle_cells):
+    def __build_graph_from_mesh(self, nodes, triangle_cells):
         graph = nx.Graph()
         for i, n in enumerate(nodes):
             graph.add_node(i, pos=n, color=(0.5, 0.5, 0.5))
@@ -32,7 +31,30 @@ class GraphMap:
 
                 weight = self.__distance_btw_points(graph.node[p1]['pos'], graph.node[p2]['pos'])
                 graph.add_edge(p1, p2, weight=weight)
-        return graph
+
+        self._graph = graph
+        self.__clean()
+
+        if self._cache:
+            self.save()
+
+    def display(self):
+        print(nx.get_node_attributes(self._graph, 'color').values())
+        #  nx.draw_networkx(self._graph, nx.get_node_attributes(self._graph, 'pos'),
+                #  node_color=nx.get_node_attributes(self._graph, 'color').values())
+        nx.draw(self._graph, nx.get_node_attributes(self._graph, 'pos'))
+        plt.show()
+
+    def read_cache(self):
+        return nx.read_gpickle(self._cache_path)
+
+    def save(self):
+        nx.write_gpickle(self._graph, self._cache_path)
+
+    def __distance_btw_points(self, p1, p2):
+        x = (p1[0] - p2[0])**2
+        y = (p1[1] - p2[1])**2
+        return math.sqrt(x + y)
 
     def __merge_nodes(self, node, old_node):
         self._graph.node[node]['color'] = (0, 0, 0)
